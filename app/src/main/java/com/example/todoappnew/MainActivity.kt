3a4e906bb279816d48d9f3b69b2e48cb578982ce
Jakub Firkowski <firkowskikuba@gmail.com>
2025-03-08 23:42:16 +0100

    Dodanie Drugiego TEXT

--- app/src/main/java/com/example/todoappnew/MainActivity.kt
+++ app/src/main/java/com/example/todoappnew/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.todoappnew.ui.theme.ToDoAppNewTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +33,14 @@ class MainActivity : ComponentActivity() {
     {
         Box(modifier = Modifier.fillMaxSize())
         {
-           Text(text = "First Commit", fontSize = 26.sp ,modifier = Modifier.align(Alignment.Center))
+            Column(modifier = Modifier.align(Alignment.Center),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally)
+            {
+                Text(text = "First Commit", fontSize = 26.sp )
+                Text(text = "Second Commit", fontSize = 26.sp )
+            }
+
         }
 
     }
